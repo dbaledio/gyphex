0df3abc06fd15e51a40ad8664dca416c10765757
Danilo Baledio <d_baledio@u.pacific.edu>
2020-08-18 21:29:12 -0700

    Added nav bar to top of app post-login. Buttons not functioning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react'
+import { Auth } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  appBarSpacer: {
+    marginRight: theme.spacing(2),
+    flexGrow: 1,
+  },
+}));
+
+
+async function signOut() {
+    try {
+        await Auth.signOut();
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+}
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Button variant="contained" color="primary" disableElevation>
+            Go Back
+          </Button>
+          <div className={classes.appBarSpacer}>
+          </div>
+          <AmplifySignOut />
+        </Toolbar>
+      </AppBar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -24,4 +61,4 @@ function App() {
   );
 }
 
-export default withAuthenticator(App);
+export default withAuthenticator(App, true);
