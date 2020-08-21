12706516e47facd5621a0fa7b289354b8d1d6df9
Danilo Baledio <d_baledio@u.pacific.edu>
2020-08-21 14:55:28 -0700

    Final MVP ready for production env and testing

--- src/App.js
+++ src/App.js
@@ -1,138 +1,181 @@
 // src/App.js
-import React, { Component }  from 'react';
-
-import { API, graphqlOperation, Auth } from 'aws-amplify'
-
-import { listItems as ListItems } from './graphql/queries'
+import React, { Component } from 'react';
+import { API, graphqlOperation, Auth } from 'aws-amplify';
+import { listItems as ListItems } from './graphql/queries';
 // import the mutation
-import { createItem as CreateItem } from './graphql/mutations'
-
+import { createItem as CreateItem } from './graphql/mutations';
 // src/App.js, import the new component
-import { withAuthenticator, AmplifyAuthenticator } from '@aws-amplify/ui-react'
-
-import ItemCard from './ItemCard'
-import AppBar from './AppBar'
-
-
-
-
-
-
+import { withAuthenticator, AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import AppBar from './AppBar';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
+import { spacing } from '@material-ui/system';
+import {
+  Card,
+  CardContent,
+  Button,
+  Typography,
+  Input,
+  Grid,
+} from '@material-ui/core/';
+
+const useStyles = makeStyles(theme => ({
   pos: {
     marginBottom: 12,
   },
-});
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+    height: "100%",
 
+  },
+  form: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+    textAlign:'center'
+   },
+}));
 
 class App extends Component {
   // define some state to hold the data returned from the API
   state = {
-    name: '', category: '', description: '', items: []
-  }
+    name: '',
+    category: '',
+    description: '',
+    items: [],
+  };
 
   // execute the query in componentDidMount
   async componentDidMount() {
-    const user = await Auth.currentAuthenticatedUser()
-    console.log('user:', user)
-    console.log('user info:', user.signInUserSession.idToken.payload)
+    const user = await Auth.currentAuthenticatedUser();
+    console.log('user:', user);
+    console.log('user info:', user.signInUserSession.idToken.payload);
     try {
-      const itemData = await API.graphql(graphqlOperation(ListItems))
-      console.log('itemData:', itemData)
+      const itemData = await API.graphql(graphqlOperation(ListItems));
+      console.log('itemData:', itemData);
       this.setState({
-        items: itemData.data.listItems.items
-      })
+        items: itemData.data.listItems.items,
+      });
     } catch (err) {
-      console.log('error fetching items...', err)
+      console.log('error fetching items...', err);
     }
   }
-  createItem = async() => {
-    const { name, category, description } = this.state
-    if (name === '' || category=='' || description === '') return
+  createItem = async () => {
+    const { name, category, description } = this.state;
+    if (name === '' || category == '' || description === '') return;
 
-    const item = { name, category, description }
-    const items = [...this.state.items, item]
+    const item = { name, category, description };
+    const items = [...this.state.items, item];
     this.setState({
-      items, name: '', category: '', description: ''
-    })
+      items,
+      name: '',
+      category: '',
+      description: '',
+    });
 
     try {
-      await API.graphql(graphqlOperation(CreateItem, { input: item }))
-      console.log('item created!')
+      await API.graphql(graphqlOperation(CreateItem, { input: item }));
+      console.log('item created!');
     } catch (err) {
-      console.log('error creating item...', err)
+      console.log('error creating item...', err);
     }
-  }
+  };
   onChange = (event) => {
     this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
   render() {
     return (
       <>
         <AppBar />
-        <Card className={this.props.classes.root}>
-            <CardContent>
-        <input
-          name='name'
-          onChange={this.onChange}
-          value={this.state.name}
-          placeholder='item'
-        />
-        <input
-          name='category'
-          onChange={this.onChange}
-          value={this.state.catgeory}
-          placeholder='recipient'
-        />
-        <input
-          name='description'
-          onChange={this.onChange}
-          value={this.state.description}
-          placeholder='notes'
-        />
-        <button onClick={this.createItem}>Add Item</button>
-        </CardContent>
-<CardActions>
-  <Button size="small">Learn More</Button>
-</CardActions>
-</Card>
-        {
-          this.state.items.map((item, index) => <ItemCard key={index} name={item.name} category={item.category} description={item.description} />
-          )
-        }
+        <div className={this.props.classes.root}>
+        <Grid
+          container
+          spacing={2}
+          direction='row'
+          justify='flex-start'
+          alignItems='flex-start'
+        >
+          <Grid item xs={12} sm={6} md={3}>
+            <Card className={this.props.classes.form}>
+              <CardContent                   >
+                <form
+                  noValidate
+                  autoComplete='off'
+                >
+                  <Input
+                    required
+                    name='name'
+                    onChange={this.onChange}
+                    value={this.state.name}
+                    placeholder='item'
+                    fullWidth='true'
+                  />
+                  <br></br>
+                  <Input
+                    required
+                    name='category'
+                    onChange={this.onChange}
+                    value={this.state.catgeory}
+                    placeholder='recipient'
+                    fullWidth='true'
+                  />
+                  <br></br>
+                  <Input
+                    required
+                    name='description'
+                    onChange={this.onChange}
+                    value={this.state.description}
+                    placeholder='notes'
+                    fullWidth='true'
+                  />
+                  <br></br>
+                </form>
+                <Button
+                  variant='contained'
+                  color='primary'
+                  onClick={this.createItem}
+                  fullWidth='true'
+                  disableElevation
+                >
+                  Add Item
+                </Button>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          {this.state.items.map((item, index) => (
+            <Grid item xs={12} sm={6} md={3} key={index}>
+              <Card className={this.props.classes.form}>
+                <CardContent>
+                  <Typography variant='h5' component='h2'>
+                    {item.name}
+                  </Typography>
+                  <Typography
+                    className={this.props.classes.pos}
+                    color='textSecondary'
+                  >
+                    {item.category}
+                  </Typography>
+                  <Typography variant='body2' component='p'>
+                    {item.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+        </div>
       </>
-    )
+    );
   }
 }
-
-//export default withAuthenticator(App)
-
 
 export default () => {
   const classes = useStyles();
   return (
     <AmplifyAuthenticator>
-      <App classes = {classes}/>
+      <App classes={classes} />
     </AmplifyAuthenticator>
-  )
-}
-  
+  );
+};
